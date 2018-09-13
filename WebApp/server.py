f923cf336b649c7f52a34d4c1bc2ec6a8ed7244e
jeremyjkerby <cyb3rph4n70m@gmail.com>
2018-09-13 01:08:08 -0700

    updating webapp and readme

--- WebApp/server.py
+++ WebApp/server.py
@@ -4,7 +4,7 @@
 """
 import controller as c
 import datetime, json, jwt
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify, make_response, request as req
 from functools import wraps
 
 
@@ -15,9 +15,8 @@ app = Flask('/')
 def token_required(d):
     @wraps(d)
     def decorated(*args, **kwargs):
-        if 'Authorization' in request.headers:
-            #token = request.args.get('token')
-            token = request.headers.get('Authorization')[6:]
+        if 'Authorization' in req.headers:
+            token = req.headers.get('Authorization')[6:]
             try:
                 data = jwt.decode(token, secret)
             except:
@@ -30,76 +29,49 @@ def token_required(d):
 @app.route("/user", methods = ['GET', 'POST', 'DELETE'])
 @token_required
 def user():
-    if request.method == 'GET':
-        if 'email' in request.args:
-            user = c.getUserDB(request.args['email'])
-            jdata = jsonify({user[0]: {'email': user[1], 'password': user[2], 'fname': user[3], 'lname': user[4]}})
-            return make_response(jdata, 200)
-        else:
-            return make_response("HTTP/1.1 400 - Bad Request\n", 400)
-
-    elif request.method == 'POST':
-        if request.headers['Content-Type'] == 'application/json':
-            data = request.json
-            c.addUserDB((data['email'], data['password'], data['fname'], data['lname']))
-            return make_response("HTTP/1.1 201 - Created\n", 201)
-        else:
-            return make_response("HTTP/1.1 400 - Bad Request\n", 400)
-
-    elif request.method == 'DELETE':
-        if 'email' in request.args:
-            c.deleteUserDB(request.args['email'])
-            return make_response("HTTP/1.1 200 - Ok\n", 200)
-        else:
-            return make_response("HTTP/1.1 400 - Bad Request\n", 400)
+    if req.method == 'GET' and 'email' in req.args:
+        user = c.getUserDB(req.args['email'])
+        jdata = jsonify({user[0]: {'email': user[1], 'password': user[2], 'fname': user[3], 'lname': user[4]}})
+        return make_response(jdata, 200)
+    elif req.method == 'POST' and req.headers['Content-Type'] == 'application/json':
+        data = req.json
+        c.addUserDB((data['email'], data['password'], data['fname'], data['lname']))
+        return make_response("HTTP/1.1 201 - Created\n", 201)
+    elif req.method == 'DELETE' and 'email' in req.args:
+        c.deleteUserDB(req.args['email'])
+        return make_response("HTTP/1.1 200 - Ok\n", 200)
     else:
-        return make_response("HTTP/1.1 405 - Method Not Allowed\n", 405)
+        return make_response("HTTP/1.1 400 - Bad Request\n", 400)
 
 @app.route("/workouts", methods = ['GET', 'POST', 'DELETE'])
 @token_required
 def workouts():
-    if request.method == 'GET':
-        if 'u_id' in request.args:
-            workouts = c.getAllWorkoutsDB(request.args.get('u_id'))
-            return make_response(jsonify(workouts), 200)
-        else:
-            return make_response("HTTP/1.1 400 - Bad Request\n", 400)
-
-    elif request.method == 'POST':
-        if request.headers['Content-Type'] == 'application/json':
-            workouts = []
-            data = request.json
-            for d in data:
-                workouts.append((d['u_id'], d['date'], d['type'], d['duration'], d['calories'], d['distance'], d['notes']))
+    if req.method == 'GET' and 'u_id' in req.args:
+        workouts = c.getAllWorkoutsDB(req.args.get('u_id'))
+        return make_response(jsonify(workouts), 200)
+    elif req.method == 'POST' and req.headers['Content-Type'] == 'application/json':
+        workouts = []
+        data = req.json
+        for d in data:
+            workouts.append((d['u_id'], d['date'], d['type'], d['duration'], d['calories'], d['distance'], d['notes']))
             c.addAllWorkoutsDB(workouts)
-            return make_response("HTTP/1.1 201 - Created\n", 201)
-        else:
-            return make_response("HTTP/1.1 400 - Bad Request\n", 400)
-
-    elif request.method == 'DELETE':
-        if 'u_id' in request.args:
-            c.deleteAllWorkoutsDB(request.args['u_id'])
-            return make_response("HTTP/1.1 200 - Ok\n", 200)
-        else:
-            return make_response("HTTP/1.1 400 - Bad Request\n", 400)
+        return make_response("HTTP/1.1 201 - Created\n", 201)
+    elif req.method == 'DELETE' and 'u_id' in req.args:
+        c.deleteAllWorkoutsDB(req.args['u_id'])
+        return make_response("HTTP/1.1 200 - Ok\n", 200)
     else:
-        return make_response("HTTP/1.1 405 - Method Not Allowed\n", 405)
+        return make_response("HTTP/1.1 400 - Bad Request\n", 400)
 
 @app.route("/signup", methods = ['POST'])
 def signup():
-    if request.method == 'POST':
-        if request.headers['Content-Type'] == 'application/json':
-            data = request.json
-            if data[0]['email'] and data[0]['password']:
-                token = jwt.encode({'user' : data[0]['email'], 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=60)}, secret)
-                # save email, encrypted password, fname, lname
-                return jsonify({'token': token.decode('UTF-8')})
-            else:
-                return make_response("HTTP/1.1 400 - Bad Request\n", 400)
-        else:
-            return make_response("HTTP/1.1 400 - Bad Request\n", 400)
+    if req.method == 'POST' and req.headers['Content-Type'] == 'application/json' and \
+            set(('email', 'password', 'fname', 'lname')) <= req.json[0].keys():
+        data = req.json[0]
+        token = jwt.encode({'user' : data['email'], 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=60)}, secret)
+        #TODO save email, encrypted password, fname, lname
+        return jsonify({'token': token.decode('UTF-8')})
     else:
-        return make_response("HTTP/1.1 405 - Method Not Allowed\n", 405)
+        return make_response("HTTP/1.1 400 - Bad Request\n", 400)
 
 def setup():
     c.setupDB()
